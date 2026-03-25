5fd15459ba117ff4160ae614c4e03e9ebb8acc95
Tamana365 <tamana.heela@gmail.com>
2026-03-24 22:53:14 -0400

    2nd commit

--- src/test/java/Hw01.java
+++ src/test/java/Hw01.java
@@ -4,5 +4,7 @@ public class Hw01 {
         System.out.println("hello world");
         System.out.println("hello world to all");
         // first commit
+
+        System.out.println("1,2,3,4,5,6,7");
     }
 }
